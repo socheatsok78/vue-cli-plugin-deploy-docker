60421521a07eec9b3163dab42e0313acba740175
Socheat Sok <alex@socheat.net>
2020-06-07 17:09:47 +0700

    Update prompts.js, change name and message

--- prompts.js
+++ prompts.js
@@ -2,8 +2,8 @@ module.exports = pkg => {
     const prompts = [
         {
             type: 'input',
-            name: 'containerTag',
-            message: 'The container tag name.',
+            name: 'tag',
+            message: `Name and optionally a tag in the 'name:tag' format`,
             validate: input => !!input,
             default: `${pkg.name}:latest`
         }
